2ebdcfa7f2ced3a9067567dfdf23021f2a037600
Gonzalo Fleming <gonzafg2@gmail.com>
2021-09-01 17:37:49 -0400

    Carga inicial de proyecto

---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="">
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+  components: {},
+  data() {
+    return {};
+  },
+});
+</script>
